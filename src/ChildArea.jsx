f680d64e81bf08021161bf963797351f0c9fd957
KurumiRumi <40328870+KurumiRumi@users.noreply.github.com>
2022-07-25 17:48:50 +0900

    10. レンダリング最適化１(memo)

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,10 +1,13 @@
+import { memo } from "react";
+
 const style = {
   width: "100%",
   height: "200px",
   backgroundColor: "khaki"
 };
 
-export const ChildArea = (props) => {
+// memoで囲うとpropsが変化しない限り再レンダリングされない
+export const ChildArea = memo((props) => {
   const { open } = props;
   console.log("ChildAreaがレンダリングされた!!");
 
@@ -22,4 +25,4 @@ export const ChildArea = (props) => {
       ) : null}
     </>
   );
-};
+});
